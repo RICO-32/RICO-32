c9a91dfb3931235ede30aae43513fc42ff4e459b
Arch <rhygonfn@gmail.com>
2025-11-20 17:50:45 -0500

    Added frame rate api

--- scripts/main.lua
+++ scripts/main.lua
@@ -9,4 +9,9 @@ function update(dt)
 	if player.x % 10 == 0 then
 		log("Player's X is: " .. tostring(player.x) .. " and " .. tostring(dt) .. " time has passed")
 	end
+	if player.x % 100 == 0 then
+		local new_frame_rate = math.floor(player.x / 10)
+		log("Changing framerate to " .. tostring(new_frame_rate))
+		set_frame_rate(new_frame_rate)
+	end
 end
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use mlua::prelude::*;
-use mlua::{StdLib};
-use std::{fs, thread, time};
+use std::sync::{Arc, Mutex};
+use mlua::StdLib;
+use std::{fs, i32, thread, time};
 use std::time::Instant;
 
 pub struct ScriptEngine {
     lua: Lua,
     scripts_dir: String,
     time: Instant,
-    frame_rate: i32
+    frame_rate: Arc<Mutex<i32>>
 }
 
 impl ScriptEngine {
@@ -15,11 +16,11 @@ impl ScriptEngine {
         let options = LuaOptions::new();
         let lua = Lua::new_with(StdLib::ALL_SAFE, options).expect("Could not load lua state");
 
-        let engine = ScriptEngine {
+        let mut engine = ScriptEngine {
             lua,
             scripts_dir: scripts_dir.into(),
             time: Instant::now(),
-            frame_rate: 60
+            frame_rate: Arc::from(Mutex::from(60))
         };
 
         engine.register_api()?;
@@ -28,13 +29,24 @@ impl ScriptEngine {
         Ok(engine)
     }
 
-    fn register_api(&self) -> LuaResult<()> {
+    fn register_api(&mut self) -> LuaResult<()> {
         let globals = self.lua.globals();
 
         globals.set(
             "log",
             self.lua.create_function(|_, msg: String| {
                 println!("[Lua] {}", msg);
+                Ok(())
+            })?,
+        )?;
+
+        let frame_rate = self.frame_rate.clone();
+        globals.set(
+            "set_frame_rate",
+            self.lua.create_function(move |_, msg: String| {
+                let x = msg.parse::<i32>()
+                    .map_err(|_| mlua::Error::RuntimeError(format!("Invalid frame rate: {}", msg)))?;
+                *frame_rate.lock().unwrap() = x;
                 Ok(())
             })?,
         )?;
@@ -83,7 +95,7 @@ impl ScriptEngine {
         let now = Instant::now();
         let dt = now.duration_since(self.time).as_millis();
         self.time = now;
-        thread::sleep(time::Duration::from_millis(1000/self.frame_rate as u64));
+        thread::sleep(time::Duration::from_millis(1000/(*self.frame_rate.lock().unwrap()) as u64));
         self.call_update(dt)
     }
 
